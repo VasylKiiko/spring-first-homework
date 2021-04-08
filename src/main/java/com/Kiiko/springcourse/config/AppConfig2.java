4d990c6247a67f1f162a5355c79f8129e33c5195
Vasyl Kiiko <vasylkiikofw@gmail.com>
2021-04-08 16:10:26 +0300

    Updated AppConfig2 @ComponentScan annotation

--- src/main/java/com/Kiiko/springcourse/config/AppConfig2.java
+++ src/main/java/com/Kiiko/springcourse/config/AppConfig2.java
@@ -1,22 +1,15 @@
 package com.Kiiko.springcourse.config;
 
 import com.Kiiko.springcourse.beans2.CatAnimal;
-import com.Kiiko.springcourse.beans3.BeanD;
-import com.Kiiko.springcourse.beans3.BeanF;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import com.Kiiko.springcourse.beans3.BeanE;
+import org.springframework.context.annotation.*;
 
 @Configuration
-@ComponentScan(basePackageClasses = CatAnimal.class)
+@ComponentScans({
+        @ComponentScan(basePackageClasses = CatAnimal.class),
+        @ComponentScan(value = "com.Kiiko.springcourse.beans3",
+                excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = BeanE.class)
+        )
+})
 public class AppConfig2 {
-    @Bean
-    public BeanD beanD() {
-        return new BeanD();
-    }
-
-    @Bean
-    public BeanF beanF() {
-        return new BeanF();
-    }
 }
